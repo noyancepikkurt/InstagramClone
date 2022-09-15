8f480000106f7db3d33f2ea5e155b0aa5db23634
Noyan Çepikkurt <cepikkurtnoyan@hotmail.com>
2022-09-15 17:59:46 +0300

    Initial Commit

---
+++ InstagramCloneFBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramCloneFBase
+//
+//  Created by Noyan Çepikkurt on 15.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
