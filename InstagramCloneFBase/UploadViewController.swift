16e6e0667ef5343e588bffd43d0e21a6479391b1
Noyan Çepikkurt <cepikkurtnoyan@hotmail.com>
2022-09-19 17:13:44 +0300

    Veritabanı öncesi gerekli kodlar yazıldı. Seçilen fotoğraflar Firebase'e yükleniyor ve UUID.String oluşturuyor.

--- InstagramCloneFBase/UploadViewController.swift
+++ InstagramCloneFBase/UploadViewController.swift
@@ -6,15 +6,82 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseCore
+import FirebaseStorage
 
-class UploadViewController: UIViewController {
+class UploadViewController: UIViewController , UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var uploadButton: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        imageView.addGestureRecognizer(gestureRecognizer)
     }
     
-
+    @objc func chooseImage() {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true)
+        
+        }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true)
+    }
+    
+    func makeAlert(titleInput : String, messageInput : String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
+    }
+    
+    
+    
+    @IBAction func uploadButtonClicked(_ sender: Any) {
+        
+        let uuid = UUID().uuidString
+        
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        
+        let mediaFolder = storageReference.child("media")
+        
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5) {
+            
+            let imageReference = mediaFolder.child("\(uuid).jpg")
+            imageReference.putData(data, metadata: nil) { metadata, error in
+                if error != nil {
+                    self.makeAlert(titleInput: "ERROR", messageInput: error?.localizedDescription ?? "Error")
+                } else {
+                    
+                    imageReference.downloadURL { url, error in
+                        
+                        if error == nil {
+                            
+                            let imageUrl = url?.absoluteString
+                            
+                            // DATABASE
+                            
+                            
+                            
+                        }
+                    }
+                    
+                }
+            }
+        }
+        
+    }
+    
 
 }
